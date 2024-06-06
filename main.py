e570cbf9d0a1e4ff2e776c5c56bb0020cc975edb
Bashamega <adambashaahmednaji@gmail.com>
2024-06-06 18:36:43 +0300

    Final Commit 🚀

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import markdown
 # Function to create and write a file
 def generate_file(file_name, content):
     with open(file_name, 'w') as file:
@@ -15,11 +16,20 @@ def main():
                     <img src="template/assets/img/post-bg.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{file_name.split('.')[0]}</h5>
-                        <a href="/post/{file_name.split('.')[0]}" class="btn btn-primary w-full">Open</a>
+                        <a href="/posts/{file_name.split('.')[0]}" class="btn btn-primary w-full">Open</a>
                     </div>
                 </div>
             </div>
             """
+            with open('template/post.html', 'r') as post:
+                md_content = markdown.markdown(file.read())
+                file_content = post.read().replace('$post_title', file_name.split('.')[0]).replace('$Heading_title', file_name.split('.')[0]).replace('$content', md_content)
+                try:
+                    generate_file('posts/' + file_name.split('.')[0] + "/index.html", file_content)
+                except FileNotFoundError as e:
+                    #print(f"FileNotFoundError: {e}")
+                    os.makedirs('posts/' + file_name.split('.')[0])
+                    generate_file('posts/' + file_name.split('.')[0] + "/index.html", file_content)
     #print(content)
 
     page_content = ""
